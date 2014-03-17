1a62dccbeda4b26b6805bb62e3f053eef798958c
Karlis Venters <kaarlis.venters@gmail.com>
2014-03-17 19:14:19 +0000

    Read file path when instatianting the learner

--- lib/taxi_learner.rb
+++ lib/taxi_learner.rb
@@ -3,7 +3,17 @@ require 'bundler/setup'
 require 'require_all'
 
 require_all 'lib'
-# require your gems as usual
 
 class TaxiLearner
+  attr_reader :file
+
+  def initialize(argv)
+    @file = parse_file(ARGV.first)
+  end
+
+private
+
+  def parse_file(path)
+    path
+  end
 end
---
+++ spec/taxi_learner_spec.rb
@@ -0,0 +1,24 @@
+describe TaxiLearner do
+
+  describe 'input parsing' do
+    subject { TaxiLearner }
+    context 'fails' do
+      it 'without any params' do
+        expect(subject.new).to raise_error
+      end
+
+      it 'with an invalid file path' do
+        ARGV[0] = 'no/file/here' 
+        expect(subject.new(ARGV)).to raise_error
+      end
+    end
+
+    context 'succeeds' do
+      it 'with a correct filename' do
+        pending
+        ARGV[0] = 'spec/fixtures/input.yml'
+        expect(subject.new(ARGV).file).to eq('something')
+      end
+    end
+  end
+end
