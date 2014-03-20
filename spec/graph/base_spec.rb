b9abe28677e4164ba60f184883d4a98643e72537
Karlis Venters <kaarlis.venters@gmail.com>
2014-03-20 11:12:05 +0000

    Refactor graph specs

--- spec/graph/base_spec.rb
+++ spec/graph/base_spec.rb
@@ -4,6 +4,8 @@ describe TaxiLearner::Graph::Base do
   subject { TaxiLearner::Graph::Base }
 
   describe 'initialisation' do
+    let(:internal_graph) { Plexus::Digraph.new() }
+
     it 'raises error on bad empty matrix' do
       expect{ subject.new([])
         }.to raise_error ArgumentError, 'bad matrix dimensions: []'
@@ -15,37 +17,34 @@ describe TaxiLearner::Graph::Base do
     end
 
     it 'ignores self loops' do
-      internal_graph = Plexus::Digraph.new
       internal_graph.add_edge!(1,2,1).add_edge!(2,1,1)
       expect(subject.new([[1,1],[1,1]]).graph).to eq(internal_graph)
     end
+
+    context 'with correct input adjacency matrix' do
+      it 'initialises correctly' do
+        matrix = [[0, 1, 2],
+                  [1, 0, 1],
+                  [0, 3, 1]]
+        internal_graph.add_edge!(1,2,1)
+                      .add_edge!(1,3,2)
+                      .add_edge!(2,1,1)
+                      .add_edge!(2,3,1)
+                      .add_edge!(3,2,3)                  
+        expect(subject.new(matrix).graph).to eq(internal_graph)
+      end
+    end
   end
 
-  context 'with correct input adjacency matrix' do
-    let(:internal_graph) { Plexus::Digraph.new() }
-    let(:matrix) { [[0, 1, 2], #directed weighted graph
-                    [1, 0, 1],
-                    [0, 3, 1]] }
-    subject { TaxiLearner::Graph::Base.new(matrix) }
+  describe '#path_weight' do
+    let(:graph) { subject.new([[0, 1, 2],
+                               [1, 0, 1],
+                               [0, 3, 0]]) }
 
-    before do
-      internal_graph.add_edge!(1,2,1)
-                    .add_edge!(1,3,2)
-                    .add_edge!(2,1,1)
-                    .add_edge!(2,3,1)
-                    .add_edge!(3,2,3)
-     end
-
-    it 'initialises correctly' do
-      expect(subject.graph).to eq(internal_graph)
-    end
-
-    describe '#path_weight' do      
-      it 'returns a weight for the shortest path' do
-        expect(subject.path_weight(1,2)).to eq(1)
-        expect(subject.path_weight(1,3)).to eq(2)
-        expect(subject.path_weight(3,1)).to eq(4)
-      end
+    it 'returns a weight for the shortest path' do
+      expect(graph.path_weight(1,2)).to eq(1)
+      expect(graph.path_weight(1,3)).to eq(2)
+      expect(graph.path_weight(3,1)).to eq(4)
     end
   end
 end
