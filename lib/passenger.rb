6a1cc0df0be10927553dee353261cceef1373b34
Karlis Venters <kaarlis.venters@gmail.com>
2014-03-21 16:38:07 +0000

    Calculate passenger's probabilistic value

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,17 +1,32 @@
 module TaxiLearner
   class Passenger
-    attr_reader :destination, :location, :price, :world
+    attr_reader :destination, :location, :price, :world, :characteristics
 
-    def initialize(world:, location:, price:)
+    def initialize(world:, location:, price:, characteristics: [])
       @world = world
       @location = location
       @destination = world.graph.random_vertex
       @price = price
+      @characteristics = characteristics
     end
 
     def expected_fare
       distance = @world.graph.distance(@location, @destination)
       distance * price
+    end
+
+    def probabilistic_value
+      sum = 0
+      total_weight = 0
+      @characteristics.each do |c|
+        total_weight += c.weight
+      end
+
+      @characteristics.each do |c|
+        relative_weight = c.weight / total_weight
+        sum += relative_weight * c.normalised_value
+      end
+      sum
     end
   end
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -3,12 +3,15 @@ require 'plexus'
 describe TaxiLearner::Passenger do
   let(:graph) { double(random_vertex: 'destination_vertex') }
   let(:world) { double(graph: graph) }
+  let(:characteristic) { double(weight: 0.5, normalised_value: 0.9) }
   subject { TaxiLearner::Passenger.new(world: world,
-                                       location: 'location_vertex',
-                                       price: 12) }
+     location: 'location_vertex',
+     price: 12,
+     characteristics: [characteristic]) }
 
   describe 'initialisation' do
-    it 'has a set of characteristics'
+    its(:characteristics) { should include(characteristic) }
+    its(:characteristics) { should be_a_kind_of(Array) }
     its(:world) { should eq(world) }
     its(:location) { should eq('location_vertex') }
     its(:destination) { should eq('destination_vertex') }
@@ -21,6 +24,12 @@ describe TaxiLearner::Passenger do
                         .with('location_vertex', 'destination_vertex')
                         .and_return(5)
       expect(subject.expected_fare).to eq(60)
+    end
+  end
+
+  describe '#probabilistic_value' do
+    it 'is calculated according to the formula' do
+      expect(subject.probabilistic_value).to eq(0.9)
     end
   end
 
