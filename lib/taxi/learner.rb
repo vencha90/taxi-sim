a8223e997ccf34f8720731844b5cfac5e579fef9
Karlis Venters <kaarlis.venters@gmail.com>
2014-03-25 14:27:15 +0000

    Add TD(0) to Learner

--- lib/taxi/learner.rb
+++ lib/taxi/learner.rb
@@ -1,12 +1,47 @@
 module TaxiLearner
   class Taxi
     class Learner
-      def initialize
+      attr_reader :value_estimates, :state, :visits
 
+      def initialize(state:, environment:, discount_factor: 0.1,
+                     step_size_function: nil, value_estimates: nil)
+        @discount_factor = discount_factor
+        @state = state
+        @states = environment
+        @visits = @states.map{ 0 }
+
+        @step_size_function = step_size_function || default_step_size_function
+        @value_estimates = value_estimates || default_value_estimates
       end
 
       def act!
 
+      end
+
+      def update!(new_state, reward) # TD(0)
+        delta = reward + 
+                @discount_factor * @value_estimates[new_state] -
+                @value_estimates[@state]
+        @value_estimates[@state] += delta * @step_size_function.call(@state)
+        @visits[@state] += 1
+        @state = new_state
+      end
+
+    private
+
+      def default_step_size_function
+        Proc.new do |state|
+          visits = @visits[state] 
+          if visits > 0
+            1.0 / visits
+          else
+            1.0
+          end
+        end
+      end
+
+      def default_value_estimates
+        @states.map{ 0.5 }
       end
     end
   end
--- spec/taxi/learner_spec.rb
+++ spec/taxi/learner_spec.rb
@@ -1,3 +1,57 @@
 describe Taxi::Learner do
-  it { should respond_to :act! }
+  let(:min_params) { {state: 2, environment: [1, 2, 3, 4]} }
+  subject { Taxi::Learner.new(min_params)}
+
+  describe 'initialisation' do
+    it 'instantiates visited states' do
+      expect(subject.visits).to eq([0, 0, 0, 0])
+    end
+
+    it 'sets a default discount factor' do
+      expect(subject.instance_variable_get '@discount_factor').to eq(0.1)
+    end
+
+    it 'sets a default step size function' do
+      subject.instance_variable_set('@visits', [0, 2])
+      function = subject.instance_variable_get('@step_size_function')
+      expect(function.call 1).to eq(0.5)
+      expect(function.call 0).to eq(1)
+    end
+
+    it 'sets default value estimates' do
+      expect(subject.instance_variable_get '@value_estimates'
+        ).to eq([0.5, 0.5, 0.5, 0.5])
+    end
+  end
+
+  describe '#update!' do
+    subject { Taxi::Learner.new(state: 0,
+      environment: [0, 1, 2],
+      discount_factor: 0.2,
+      step_size_function: double(call: 0.5),
+      value_estimates: [0.3, 0.3, 0.3]
+    )}
+
+    let(:update!) { subject.update!(2, 0.5) }
+
+    it 'updates value estimates correctly' do
+      expect{ update! 
+        }.to change{ subject.value_estimates[0] }.from(0.3)
+        .to be_within(0.000001).of(0.43)
+    end
+
+    it 'updates visits of old state' do
+      expect{ update! }.to change{ subject.visits[0] }.from(0).to(1)
+    end
+
+    it 'sets old state to new state' do
+      expect{ update! }.to change{ subject.state }.from(0).to(2)
+    end
+  end
+
+  describe '#act!' do
+    it { should respond_to :act! }
+    it 'chooses an action'
+    it "isn't greedy"
+  end
 end
